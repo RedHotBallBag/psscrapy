9b77ef236002b1dc58923b9dd8e6a7bfbfdd88db
RedHotBallBag <47257798+RedHotBallBag@users.noreply.github.com>
2024-05-10 22:19:16 +0100

    Update gamespider.py
    added signal

--- gamescraper/gamescraper/spiders/gamespider.py
+++ gamescraper/gamescraper/spiders/gamespider.py
@@ -23,6 +23,14 @@ class GameSpider(scrapy.Spider):
             self.logger.info('Following next page: %s', next_page)
             yield response.follow(next_page, callback=self.parse)
 
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(GameSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.closed, signal=signals.spider_closed)
+        return spider
+    
+    
     def closed(self, reason):
         # Define source and destination paths
         src_path = '/path/to/output.csv'
